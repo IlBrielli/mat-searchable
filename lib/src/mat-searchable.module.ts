7181d3d435534d1c1d848389408092636a413eaa
Alessio Gabrielli <alessio>
2021-01-22 17:06:19 +0100

    update material 10

--- lib/src/mat-searchable.module.ts
+++ lib/src/mat-searchable.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSelectModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatSearchableDirective } from './mat-searchable.directive';
